a7512f65655be288a2f8eea66c34144c715cbd34
Mathew Benson <mbenson@windhoverlabs.com>
2018-06-30 23:07:54 +0000

    Merge pull request #2 in PUB/yamcs-cfs from release/candidate to master
    Squashed commit of the following:
    
    commit f5c58dfe2729caedcff7b9ec77db17e5deb90405
    Merge: 85125cf 5f4b20b
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Sat Jun 30 22:45:49 2018 +0000
    
        Merge pull request #1 in PUB/yamcs-cfs from develop to release/candidate
    
        * commit '5f4b20b61285d6b689a0d78c16ea6035f8cbb3c7':
          Renaming code and adding status.
          Fixed yamcs version to the currently passing version, 3.2.2
          Fixed breaks due to upgrade to yamcs 3.2.3.
          Stepped up from yamcs 3.2.1 to 3.2.3
    
    commit 5f4b20b61285d6b689a0d78c16ea6035f8cbb3c7
    Merge: 5353e54 9f4dc85
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Tue Mar 13 18:01:26 2018 -0500
    
        Added the internal 'SetVehicleAddress' CCSDS command to the plugin.  Must now set gndSysApid to an unused APID.  The 'setVehicleAddress' command is command code 0.  The only parameter is a max 256 byte address.
    
    commit 9f4dc8569a061a23dc01949fcfde514152693680
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Tue Mar 13 14:50:34 2018 -0500
    
        Renaming code and adding status.
    
    commit 5353e543bef71b59fc90643319559faecbb88fb5
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Sun May 21 22:01:04 2017 -0500
    
        Fixed yamcs version to the currently passing version, 3.2.2
    
    commit 2f364db98b24c2d2fdb8a3a758cc8c64cd398cf6
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Sun May 21 16:07:34 2017 -0500
    
        Fixed breaks due to upgrade to yamcs 3.2.3.
    
    commit 94a737034ec6acbb4f5e0cb567c10a3e2915e385
    Author: Mathew Benson <mbenson@windhoverlabs.com>
    Date:   Sun May 21 11:58:23 2017 -0500
    
        Stepped up from yamcs 3.2.1 to 3.2.3

--- src/main/java/com/windhoverlabs/yamcs/tctm/CfsTlmPacket.java
+++ src/main/java/com/windhoverlabs/yamcs/tctm/CfsTlmPacket.java
@@ -1,4 +1,4 @@
-package com.odysseysr.proteus.yamcs.tctm;
+package com.windhoverlabs.yamcs.tctm;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,7 +6,7 @@ import java.nio.ByteBuffer;
 
 import org.yamcs.utils.CcsdsPacket;
 import org.yamcs.utils.TimeEncoding;
-import com.odysseysr.proteus.yamcs.tctm.CfsUdpTmProvider;
+import com.windhoverlabs.yamcs.tctm.CfsUdpTmProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -332,6 +332,7 @@ public class CfsTlmPacket implements Comparable<CfsTlmPacket>
     {
         return getAPID(ByteBuffer.wrap(packet));
     }
+	
 	
     public static int getCccsdsPacketLength(byte[] buf) 
     {       
