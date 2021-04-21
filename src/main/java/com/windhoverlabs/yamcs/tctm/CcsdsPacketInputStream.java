1796e77e63d332095465bb126c6f5f582de436c9
Mathew Benson <mbenson@windhoverlabs.com>
2021-04-20 20:42:55 -0500

    Completed the new tctm plugin.

--- src/main/java/com/windhoverlabs/yamcs/tctm/CcsdsPacketInputStream.java
+++ src/main/java/com/windhoverlabs/yamcs/tctm/CcsdsPacketInputStream.java
@@ -1,10 +1,44 @@
-package org.yamcs.tctm;
+/****************************************************************************
+ *
+ *   Copyright (c) 2021 Windhover Labs, L.L.C. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name Windhover Labs nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *****************************************************************************/
+
+package com.windhoverlabs.yamcs.tctm;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.yamcs.YConfiguration;
+import org.yamcs.tctm.PacketInputStream;
 
 /**
  * Reads CCSDS packets from an input stream:
@@ -13,14 +47,15 @@ import org.yamcs.YConfiguration;
  * 
  * It also support a maxLength property to limit the size of the packet that is being read.
  * 
- * @author nm
+ * @author Mathew Benson
  *
  */
 public class CcsdsPacketInputStream implements PacketInputStream {
     DataInputStream dataInputStream;
-    int maxPacketLength = 32768;
+    int maxPacketLength    = 32768;
     boolean secHdrRequired = false;
-    boolean segAllowed = false;
+    boolean segAllowed     = false;
+    boolean tlmOnly        = false;
     
     enum ParserState {
     	WAITING_FOR_BYTE_1,
@@ -36,6 +71,7 @@ public class CcsdsPacketInputStream implements PacketInputStream {
         this.maxPacketLength = args.getInt("maxPacketLength", maxPacketLength);
         this.secHdrRequired  = args.getBoolean("secHdrRequired", secHdrRequired);
         this.segAllowed      = args.getBoolean("segAllowed", segAllowed);
+        this.tlmOnly         = args.getBoolean("tlmOnly", tlmOnly);
     }
 
     @Override
@@ -57,10 +93,21 @@ public class CcsdsPacketInputStream implements PacketInputStream {
     		        	break;
     		        }
     		        
+    		        /* If the telemetry only is selected, check it. */
+    		        if(this.tlmOnly) {
+    		        	/* Telemetry only. */
+    		            if((hdr[0] & 0x10) != 0) {
+    		        	    /* The downlink should contain telemetry only. 
+    		        	     * The stream is out of sync so remain in this 
+    		        	     * state. */
+    		        	    break;
+    		            }
+    		        }
+    		        
     		        /* If the Secondary Header is required, check it. */
     		        if(this.secHdrRequired) {
     		        	/* It is required. */
-    		            if((hdr[0] & 0x08) != 1) {
+    		            if((hdr[0] & 0x08) != 0x08) {
     		        	    /* The secondary header is required but is not 
     		        	     * present.  The stream is out of sync so remain 
     		        	     * in this state. */
@@ -80,7 +127,7 @@ public class CcsdsPacketInputStream implements PacketInputStream {
     		         * segmentation flags. */
     		        if(this.segAllowed == false) {
     		        	/* It is not allowed. */
-    		            if((hdr[0] & 0xc0) != 0) {
+    		            if((hdr[2] & 0xc0) != 0xc0) {
     		        	    /* The segmentation flags must be 3 (complete packet). 
     		        	     * The stream is out of sync, so fall back to the 
     		        	     * initial state. */
@@ -115,7 +162,7 @@ public class CcsdsPacketInputStream implements PacketInputStream {
     		    }	
     		}
     	}
-    	
+        
         return packet;
     }
 
