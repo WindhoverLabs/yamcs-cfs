40d5d2355b41303e02c67c7f818eaf32629d3979
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-05-04 17:31:51 -0500

    -Query mdb

--- src/main/java/com/windhoverlabs/yamcs/studio/HelloWorldApi.java
+++ src/main/java/com/windhoverlabs/yamcs/studio/HelloWorldApi.java
@@ -2,7 +2,9 @@ package com.windhoverlabs.yamcs.studio;
 
 import org.yamcs.api.Observer;
 import org.yamcs.http.Context;
-
+import org.yamcs.http.api.ManagementApi;
+import org.yamcs.xtce.XtceDb;
+import org.yamcs.xtceproc.XtceDbFactory;
 
 
 public class HelloWorldApi extends AbstractHelloWorldApi<Context> {
@@ -20,6 +22,7 @@ public class HelloWorldApi extends AbstractHelloWorldApi<Context> {
             
             HelloInfo.Builder helloInfo = HelloInfo.newBuilder();
             
+            //Make an object to send back to the client
             helloInfo.setJob("Software Engineer");
             helloInfo.setName("Lorenzo Gomez"); 
             helloInfo.setAge(25);
@@ -28,12 +31,26 @@ public class HelloWorldApi extends AbstractHelloWorldApi<Context> {
             
             hwResponse.addHelloInfo(listHWInfo);
             
+            System.out.println(request.getInstance());
+            
+            
+            //Query the database
+            String instance = ManagementApi.verifyInstance(request.getInstance());
+            XtceDb mdb = XtceDbFactory.getInstance(instance);
+            
+            System.out.println("success...");
+            
+//            System.out.println("telem from mdb:" + mdb.getParameter("/cfs//cfe_es/CFE_ES_HkPacket_t.Payload.SysLogBytesUsed"));
+            
+            System.out.println("telem from mdb2:" +  mdb.getParameters().iterator().next().getName());
+            
+            //Send response to client
             observer.complete(hwResponse.build());
         } catch (Exception e) {
             observer.completeExceptionally(e);
         }
         
 	}
-
-
+	
+	
 }
