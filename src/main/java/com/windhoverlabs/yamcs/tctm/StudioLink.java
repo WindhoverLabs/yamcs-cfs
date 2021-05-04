735f42f8de8cad5cc94b55b30c85d75bb6373900
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-05-04 11:31:29 -0500

    -Added basic protobuf config to maven

---
+++ src/main/java/com/windhoverlabs/yamcs/tctm/StudioLink.java
@@ -0,0 +1,15 @@
+package com.windhoverlabs.yamcs.tctm;
+import org.yamcs.client.YamcsClient;
+
+public class StudioLink {
+	
+	/**
+	 * 
+	 * @return num + 1
+	 */
+	public void addOne() 
+	{
+		System.out.println("Hello World");
+	}
+
+}
