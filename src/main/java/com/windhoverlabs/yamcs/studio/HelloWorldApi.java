4f6e6fcafd0c097d262e6297f54867473b1a296d
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-05-04 15:47:01 -0500

    -Added example response to http API

--- src/main/java/com/windhoverlabs/yamcs/studio/HelloWorldApi.java
+++ src/main/java/com/windhoverlabs/yamcs/studio/HelloWorldApi.java
@@ -11,26 +11,41 @@ import org.yamcs.http.Context;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.ByteString.Output;
 
-
 public class HelloWorldApi extends AbstractHelloWorldApi<Context> {
 
 	@Override
-	public void helloWorld(Context ctx, HelloWorldRequest request, Observer<HttpBody> observer) {
+	public void helloWorld(Context ctx, HelloWorldResponse request, Observer<HelloWorldResponse> observer) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World from server...");
 		
         try (Output out = ByteString.newOutput(); Writer writer = new OutputStreamWriter(out)) {
             writer.close();
-
+            
+            HelloWorldResponse.Builder hwResponse = HelloWorldResponse.newBuilder();
+            
+            ListHelloInfo.Builder listHWInfo = ListHelloInfo.newBuilder();
+            
+            HelloInfo.Builder helloInfo = HelloInfo.newBuilder();
+            
+            helloInfo.setJob("Software Engineer");
+            helloInfo.setName("Lorenzo Gomez"); 
+            helloInfo.setAge(25);
+            
+            listHWInfo.addInfo(helloInfo);
+            
+            hwResponse.addHelloInfo(listHWInfo);
+            
             HttpBody body = HttpBody.newBuilder()
                     .setContentType("1")
                     .setData(out.toByteString())
                     .build();
-            observer.complete(body);
+            
+            observer.complete(hwResponse.build());
         } catch (IOException e) {
             observer.completeExceptionally(e);
         }
+        
 	}
 
 
